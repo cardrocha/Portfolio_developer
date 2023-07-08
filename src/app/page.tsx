3dbae3aa541e3e87eb26811bf3fbf0fc1ea06f9d
Ricardo Rocha <cardrocharj@gmail.com>
2023-07-08 20:39:02 -0300

    fix: refactore folder and rename

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import About from "@/components/About";
 import HomePage from "@/components/HomePage";
-import Skills from "./components/Skills/pages";
+import Skills from "../components/Skills";
 
 export default function Home() {
   return (
---
+++ src/components/Skills.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div>
+      <h2 className="text-4xl text-white text-center mt-32 font-semibold">Minhas Habilidades</h2>
+    </div>
+  );
+};
+
+export default Skills;
