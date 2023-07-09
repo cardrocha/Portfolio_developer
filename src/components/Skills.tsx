7eb739ccbe0f32b475ecd4d49666fdc48e35c2c6
Ricardo Rocha <cardrocharj@gmail.com>
2023-07-09 10:42:46 -0300

    refactor: section skill

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,35 +1,113 @@
+'use client'
+
 import React from "react";
 import { AiFillGithub, AiFillHtml5 } from "react-icons/ai";
-import { IoLogoCss3, IoLogoJavascript, IoLogoNodejs, IoLogoSass, IoMdGitBranch } from "react-icons/io";
+import {
+  IoLogoCss3,
+  IoLogoJavascript,
+  IoLogoNodejs,
+  IoLogoSass,
+  IoMdGitBranch,
+} from "react-icons/io";
 import { BsUbuntu } from "react-icons/bs";
 import { TbBrandNextjs, TbTerminal } from "react-icons/tb";
-import { SiCypress, SiFigma, SiJest, SiReact, SiStyledcomponents, SiTailwindcss, SiTypescript, SiVuedotjs } from "react-icons/si";
+import {
+  SiCypress,
+  SiFigma,
+  SiJest,
+  SiReact,
+  SiStyledcomponents,
+  SiTailwindcss,
+  SiTypescript,
+  SiVuedotjs,
+} from "react-icons/si";
 
 const Skills = () => {
+  const html = () => {
+    <p>
+      HTML (Linguagem de Marcação de HiperTexto) é o bloco de construção mais
+      básico da web.{" "}
+    </p>;
+  };
+
   return (
     <section id="skill" className="container mx-auto">
       <h2 className="text-4xl text-white text-center mt-32 font-semibold">
         Minhas Habilidades
       </h2>
       <div className="grid grid-cols-6 gap-14 place-items-center mt-12">
-        <AiFillHtml5 className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <IoLogoCss3 className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <IoLogoJavascript className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <AiFillGithub className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <IoMdGitBranch className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <BsUbuntu className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <TbTerminal className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <SiFigma className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <IoLogoNodejs className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <IoLogoSass className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <SiReact className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <SiTypescript className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <SiStyledcomponents className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <SiJest className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <SiCypress className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <SiVuedotjs className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <TbBrandNextjs className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
-        <SiTailwindcss className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+        <div className="flex flex-col items-center hover:text-white">
+          <AiFillHtml5 className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/50 rounded-lg p-1 cursor-pointer" />
+          <p>HTML</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <IoLogoCss3 className="text-5xl text-white mb-1 hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>CSS</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <IoLogoJavascript className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>Javascript</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <AiFillGithub className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>GitHub</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <IoMdGitBranch className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>GIT</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <BsUbuntu className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>Ubuntu</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <TbTerminal className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>Terminal</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <SiFigma className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>Figma</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <IoLogoNodejs className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>NodeJs</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <IoLogoSass className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>Sass</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <SiReact className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>ReactJs</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <SiTypescript className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>Typescript</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <SiStyledcomponents className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>Styled-Components</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <SiJest className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>Jest</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <SiCypress className="text-5xl text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>Cypress</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <SiVuedotjs className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>VueJs</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <TbBrandNextjs className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>NextJs</p>
+        </div>
+        <div className="flex flex-col items-center hover:text-white">
+          <SiTailwindcss className="text-5xl mb-1 text-white hover:text-red-400 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <p>Tailwind CSS</p>
+        </div>
       </div>
     </section>
   );
