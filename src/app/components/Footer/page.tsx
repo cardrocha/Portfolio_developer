4d9162730d2dfb6dccc8db4f1164c3cbaab9d1dc
Ricardo Rocha <cardrocharj@gmail.com>
2023-07-14 09:41:58 -0300

    refactor: footer and skill sections

--- src/app/components/Footer/page.tsx
+++ src/app/components/Footer/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AiFillGithub, AiOutlineLinkedin, AiOutlineMail, AiOutlineWhatsApp } from "react-icons/ai";
+import * as A from "react-icons/ai";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -17,13 +17,13 @@ const Footer = () => {
         <hr className="bg-red-500 border-t-2 border-solid border-red-600 mt-2" />
         <div className="flex items-center justify-center gap-14 mt-4">
           <a href="https://github.com/cardrocha" target="_blank">
-            <AiFillGithub
+            <A.AiFillGithub
               className="text-5xl text-black hover:text-red-400 shadow shadow-black/100 rounded-lg p-1 cursor-pointer"
               title="Visite meu github"
             />
           </a>
           <a href="mailto: cardrocharj@gmail.com" target="_blank">
-            <AiOutlineMail
+            <A.AiOutlineMail
               className="text-5xl text-black hover:text-red-400 shadow shadow-black/100 rounded-lg p-1 cursor-pointer"
               title="Me mande um e-mail"
             />
@@ -32,7 +32,7 @@ const Footer = () => {
             href="https://www.linkedin.com/in/ricardo-rocha-ara%C3%BAjo-b8a36a18b/"
             target="_blank"
           >
-            <AiOutlineLinkedin
+            <A.AiOutlineLinkedin
               className="text-5xl text-black hover:text-red-400 shadow shadow-black/100 rounded-lg p-1 cursor-pointer"
               title="Visite meu linkedin"
             />
@@ -41,7 +41,7 @@ const Footer = () => {
             href="https://api.whatsapp.com/send?phone=5521997924360"
             target="_blank"
           >
-            <AiOutlineWhatsApp
+            <A.AiOutlineWhatsApp
               className="text-5xl text-black hover:text-red-400 shadow shadow-black/100 rounded-lg p-1 cursor-pointer"
               title="Entre em contato"
             />
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,26 +1,11 @@
 'use client'
 
 import React from "react";
-import { AiFillGithub, AiFillHtml5 } from "react-icons/ai";
-import {
-  IoLogoCss3,
-  IoLogoJavascript,
-  IoLogoNodejs,
-  IoLogoSass,
-  IoMdGitBranch,
-} from "react-icons/io";
+import * as A from "react-icons/ai";
+import * as I from "react-icons/io";
 import { BsUbuntu } from "react-icons/bs";
-import { TbBrandNextjs, TbTerminal } from "react-icons/tb";
-import {
-  SiCypress,
-  SiFigma,
-  SiJest,
-  SiReact,
-  SiStyledcomponents,
-  SiTailwindcss,
-  SiTypescript,
-  SiVuedotjs,
-} from "react-icons/si";
+import * as T from "react-icons/tb";
+import * as S from "react-icons/si";
 
 const Skills = () => {
   const html = () => {
@@ -37,23 +22,23 @@ const Skills = () => {
       </h2>
       <div className="grid grid-cols-3 sm:grid-cols-4 xl:grid-cols-6 gap-14 place-items-center mt-12">
         <div className="flex flex-col items-center hover:text-white">
-          <AiFillHtml5 className="text-5xl mb-2 text-white hover:scale-125 duration-300 hover:text-orange-400 shadow shadow-white/50 rounded-lg p-1 cursor-pointer" />
+          <A.AiFillHtml5 className="text-5xl mb-2 text-white hover:scale-125 duration-300 hover:text-orange-400 shadow shadow-white/50 rounded-lg p-1 cursor-pointer" />
           <p>HTML</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <IoLogoCss3 className="text-5xl text-white mb-2 hover:text-blue-400 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <I.IoLogoCss3 className="text-5xl text-white mb-2 hover:text-blue-400 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>CSS</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <IoLogoJavascript className="text-5xl mb-2 text-white hover:text-yellow-500 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <I.IoLogoJavascript className="text-5xl mb-2 text-white hover:text-yellow-500 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>Javascript</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <AiFillGithub className="text-5xl mb-2 text-white hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <A.AiFillGithub className="text-5xl mb-2 text-white hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>GitHub</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <IoMdGitBranch className="text-5xl mb-2 text-white hover:text-orange-500 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <I.IoMdGitBranch className="text-5xl mb-2 text-white hover:text-orange-500 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>GIT</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
@@ -61,51 +46,51 @@ const Skills = () => {
           <p>Ubuntu</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <TbTerminal className="text-5xl mb-2 text-white hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <T.TbTerminal className="text-5xl mb-2 text-white hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>Terminal</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <SiFigma className="text-5xl mb-2 text-white hover:text-purple-600 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <S.SiFigma className="text-5xl mb-2 text-white hover:text-purple-600 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>Figma</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <IoLogoNodejs className="text-5xl mb-2 text-white hover:text-green-600 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <I.IoLogoNodejs className="text-5xl mb-2 text-white hover:text-green-600 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>NodeJs</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <IoLogoSass className="text-5xl mb-2 text-white hover:text-pink-300 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <I.IoLogoSass className="text-5xl mb-2 text-white hover:text-pink-300 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>Sass</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <SiReact className="text-5xl mb-2 text-white hover:text-blue-400 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <S.SiReact className="text-5xl mb-2 text-white hover:text-blue-400 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>ReactJs</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <SiTypescript className="text-5xl mb-2 text-white hover:text-blue-600 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <S.SiTypescript className="text-5xl mb-2 text-white hover:text-blue-600 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>Typescript</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <SiStyledcomponents className="text-5xl mb-2 text-white hover:text-fuchsia-500 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <S.SiStyledcomponents className="text-5xl mb-2 text-white hover:text-fuchsia-500 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>Styled-Components</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <SiJest className="text-5xl mb-2 text-white hover:text-rose-600 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <S.SiJest className="text-5xl mb-2 text-white hover:text-rose-600 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>Jest</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <SiCypress className="text-5xl mb-2 text-white hover:text-emerald-500 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <S.SiCypress className="text-5xl mb-2 text-white hover:text-emerald-500 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>Cypress</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <SiVuedotjs className="text-5xl mb-2 text-white hover:text-green-700 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <S.SiVuedotjs className="text-5xl mb-2 text-white hover:text-green-700 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>VueJs</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <TbBrandNextjs className="text-5xl mb-2 text-white hover:text-gray-700 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <T.TbBrandNextjs className="text-5xl mb-2 text-white hover:text-gray-700 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>NextJs</p>
         </div>
         <div className="flex flex-col items-center hover:text-white">
-          <SiTailwindcss className="text-5xl mb-2 text-white hover:text-sky-600 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
+          <S.SiTailwindcss className="text-5xl mb-2 text-white hover:text-sky-600 hover:scale-125 duration-300 shadow shadow-white/100 rounded-lg p-1 cursor-pointer" />
           <p>Tailwind CSS</p>
         </div>
       </div>
